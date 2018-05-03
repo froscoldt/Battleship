14ce0c11ffce1bd0e0de0cbee12d111abaaca80b
Mark <Mark@10.50.130.232>
2018-05-03 21:10:52 +0200

    a lot of stuff?

--- src/g11/RandomPlayer.java
+++ src/g11/RandomPlayer.java
@@ -14,40 +14,24 @@ public class RandomPlayer implements BattleshipsPlayer {
     private int sizeX;
     private int sizeY;
     private Board myBoard;
+    private boolean[][] coordinatesHit = new boolean[10][10];
     ArrayList<int[]> coordinates = new ArrayList();
+    ArrayList<ArrayList<int[]>> AllPossiblePaths = new ArrayList<>();
     private boolean checkIfHit;
     private int checkAmountOfShips;
-    private int fireX = -2;
-    private int fireY = 0;
-    private int shipAtX;
-    private int shipAtY;
-    private boolean foundShip;
+    private int x, y, sum;
+    private int[] xy;
+    boolean[][] myShips = null;
 
     public RandomPlayer() {
     }
 
-    /**
-     * The method called when its time for the AI to place ships on the board
-     * (at the beginning of each round).
-     *
-     * The Ship object to be placed MUST be taken from the Fleet given (do not
-     * create your own Ship objects!).
-     *
-     * A ship is placed by calling the board.placeShip(..., Ship ship, ...) for
-     * each ship in the fleet (see board interface for details on placeShip()).
-     *
-     * A player is not required to place all the ships. Ships placed outside the
-     * board or on top of each other are wrecked.
-     *
-     * @param fleet Fleet all the ships that a player should place.
-     * @param board Board the board were the ships must be placed.
-     */
     @Override
     public void placeShips(Fleet fleet, Board board) {
         myBoard = board;
         sizeX = board.sizeX();
         sizeY = board.sizeY();
-        boolean[][] myShips = new boolean[sizeX][sizeY];
+        myShips = new boolean[sizeX][sizeY];
         for (int i = 0; i < fleet.getNumberOfShips(); ++i) {
             Ship s = fleet.getShip(i);
             Position pos;
@@ -77,131 +61,109 @@ public class RandomPlayer implements BattleshipsPlayer {
         }
     }
 
-    /**
-     * Called every time the enemy has fired a shot.
-     *
-     * The purpose of this method is to allow the AI to react to the enemy's
-     * incoming fire and place his/her ships differently next round.
-     *
-     * @param pos Position of the enemy's shot
-     */
     @Override
     public void incoming(Position pos) {
         //Do nothing
     }
 
-    /**
-     * Called by the Game application to get the Position of your shot.
-     * hitFeedBack(...) is called right after this method.
-     *
-     * @param enemyShips Fleet the enemy's ships. Compare this to the Fleet
-     * supplied in the hitFeedBack(...) method to see if you have sunk any
-     * ships.
-     *
-     * @return Position of you next shot.
-     */
     @Override
     public Position getFireCoordinates(Fleet enemyShips) {
         checkAmountOfShips = enemyShips.getNumberOfShips();
-        if (foundShip) {
-            shipAtY++;
-            return new Position(shipAtX, shipAtY);
-            // continue direction or adjust until ship is destroyed.
-        } else if (checkIfHit) {
-            shipAtX = fireX;
-            shipAtY = fireY;
-            foundShip = true;
-            return new Position(shipAtX -1, shipAtY);
-            // pick random direction to not waste our turn.
+        if (checkIfHit == true) {
+            return huntShip(enemyShips);
+        }
+        return search(enemyShips);
+    }
 
+    @Override
+    public void hitFeedBack(boolean hit, Fleet enemyShips) {
 
-        } else {
-            if (fireX < 8) {
-                fireX = fireX + 2;
-            } else if (fireX >= 8 && fireY % 2 == 1) {
-                fireX = 0;
-                fireY = fireY + 1;
-            } else if (fireX >= 8 && fireY % 2 == 0) {
-                fireX = 1;
-                fireY = fireY + 1;
+        if (hit == true && AllPossiblePaths.isEmpty()) {
+            checkIfHit = true;
+            for (int i = 0; i < 4; i++) {
+                AllPossiblePaths.add(new ArrayList());
+            }
+
+            for (int i = 1; i < 6; i++) {
+                int[] makeCoordinate = {this.x + i, this.y};
+                AllPossiblePaths.get(0).add(makeCoordinate);
+            }
+            for (int i = -1; i > -6; i--) {
+                int[] makeCoordinate = {this.x + i, this.y};
+                AllPossiblePaths.get(1).add(makeCoordinate);
+            }
+            for (int i = 1; i < 6; i++) {
+                int[] makeCoordinate = {this.x, this.y + i};
+                AllPossiblePaths.get(2).add(makeCoordinate);
+            }
+            for (int i = -1; i > -6; i--) {
+                int[] makeCoordinate = {this.x, this.y + i};
+                AllPossiblePaths.get(3).add(makeCoordinate);
             }
         }
 
-        return new Position(fireX, fireY);
-    }
+        if (hit == false && checkIfHit == true && AllPossiblePaths.size() > 0) {
+            AllPossiblePaths.remove(0);
+        }
 
-    /**
-     * Called right after getFireCoordinates(...) to let your AI know if you hit
-     * something or not.
-     *
-     * Compare the number of ships in the enemyShips with that given in
-     * getFireCoordinates in order to see if you sunk a ship.
-     *
-     * @param hit boolean is true if your last shot hit a ship. False otherwise.
-     * @param enemyShips Fleet the enemy's ships.
-     */
-    @Override
-    public void hitFeedBack(boolean hit, Fleet enemyShips) {
-        checkIfHit = hit;
-
-        if (checkAmountOfShips != enemyShips.getNumberOfShips() || hit == false) {
-            foundShip = false;
-
+        if (checkAmountOfShips != enemyShips.getNumberOfShips()) {
+            checkIfHit = false;
+            AllPossiblePaths.clear();
         }
 
     }
 
-    /**
-     * Called in the beginning of each match to inform about the number of
-     * rounds being played.
-     *
-     * @param rounds int the number of rounds i a match
-     */
+    private Position search(Fleet enemyShips) {
+        do {
+            this.x = rnd.nextInt(10);
+            this.y = rnd.nextInt(10);
+            this.sum = x + y;
+        } while ((sum % 2 == 1 || coordinatesHit[x][y] == true));
+        coordinatesHit[x][y] = true;
+        return new Position(x, y);
+
+    }
+
+    private Position huntShip(Fleet enemyShip) {
+        if (AllPossiblePaths.get(0).isEmpty()) {
+            AllPossiblePaths.remove(0);
+        }
+        int[] checkCoordinate = AllPossiblePaths.get(0).get(0);
+        AllPossiblePaths.get(0).remove(0);
+        return new Position(checkCoordinate[0], checkCoordinate[1]);
+
+    }
+
     @Override
     public void startMatch(int rounds, Fleet ships, int sizeX, int sizeY) {
 
     }
 
-    /**
-     * Called at the beginning of each round.
-     *
-     * @param round int the current round number.
-     */
     @Override
     public void startRound(int round) {
 
     }
 
-    /**
-     * Called at the end of each round to let you know if you won or lost.
-     * Compare your points with the enemy's to see who won.
-     *
-     * @param round int current round number.
-     * @param points your points this round: 100 - number of shot used to sink
-     * all of the enemy's ships.
-     *
-     * @param enemyPoints int enemy's points this round.
-     */
     @Override
     public void endRound(int round, int points, int enemyPoints) {
         //Do nothing
     }
 
-    /**
-     * Called at the end of a match (that usually last 1000 rounds) to let you
-     * know how many losses, victories and draws you scored.
-     *
-     * @param won int the number of victories in this match.
-     * @param lost int the number of losses in this match.
-     * @param draw int the number of draws in this match.
-     */
     @Override
     public void endMatch(int won, int lost, int draw) {
         //Do nothing
     }
 
     private boolean collision(Position pos, Ship s, boolean vertical) {
+        for (int j = 0; j < s.size(); j++) {
+            if (vertical && myShips[pos.x][pos.y + j] == true) {
+                return true;
+            }
+            if (myShips[pos.x + j][pos.y] == true) {
+                return true;
+            }
+        }
         return false;
-
     }
+
 }
